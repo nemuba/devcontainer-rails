3defbe5843a2f363796e040e3623d15bb9e9aab3
Alef Ojeda de Oliveira <alef.oliveira@siedos.com.br>
2024-09-06 09:16:07 -0300

    setup initial

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+if ENV["CAPYBARA_SERVER_PORT"]
+  served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
+if ENV["CAPYBARA_SERVER_PORT"]
+  served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
+
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
+    browser: :remote,
+    url: "http://:4444"
+  }
+else
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+end
+
+    browser: :remote,
+    url: "http://:4444"
+  }
+else
+if ENV["CAPYBARA_SERVER_PORT"]
+  served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
+
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
+    browser: :remote,
+    url: "http://:4444"
+  }
+else
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+end
+
+end
+end
